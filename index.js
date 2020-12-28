b05091f1b927371336221d41178e2c01f6fdce5b
jreyes8631 <jreyes8631@gmail.com>
2020-12-27 21:36:29 -0500

    commit tree 165e4607ab2403fab33e5d5d3b2d2956bc628642

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function(collection, iteratee) {
+    each: function(collection, callback) {
       const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
       for (let idx = 0; idx < newCollection.length; idx++)
-        iteratee(newCollection[idx])
+        callback(newCollection[idx])
 
       return collection
     },
 
-    map: function(collection, iteratee) {
+    map: function(collection, callback) {
       if (!(collection instanceof Array))
         collection = Object.values(collection)
 
       const newArr = []
 
       for (let idx = 0; idx < collection.length; idx++)
-        newArr.push(iteratee(collection[idx]))
+        newArr.push(callback(collection[idx]))
 
       return newArr
     },
